3aa1c1ddb2a6bc038ebe7aed049342baf54d468b
Kurt von Laven <kurt@endlessm.com>
2014-03-19 17:46:04 -0700

    Successfully linked against GLib.

--- src/eos-metrics-instrumentation.c
+++ src/eos-metrics-instrumentation.c
@@ -1,11 +1,22 @@
 /* Copyright 2014 Endless Mobile, Inc. */
 
+#include <glib.h>
 #include <stdio.h>
 #include "eos-metrics-instrumentation.h"
+
+static gboolean
+meet_and_greet (gpointer data)
+{
+    printf ("Hello World!\n");
+    return TRUE;
+}
 
 int
 main(int argc, const char const *argv[])
 {
-    printf("Hello World\n");
+    GMainLoop *main_loop = g_main_loop_new (NULL, TRUE);
+    g_timeout_add_seconds (2, meet_and_greet, NULL);
+    g_main_loop_run (main_loop);
+    g_main_loop_unref (main_loop);
     return 0;
 }
